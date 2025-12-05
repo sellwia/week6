009c719dc2045764f14cd37bd032ca533782c46a
sellwia <Sellydwikurnia@gmail.com>
2025-12-05 18:17:37 +0700

    membuat icon login

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -6,12 +6,21 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 return Scaffold(
-        body: Center(
-          child: Text(
-            "LOGIN",
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            child: const Column(
+              children: [
+                SizedBox(height: 10),
+                Icon(
+                  Icons.lock_outline,
+                  size: 80,
+                  color: Colors.amber,
+                ),
+
+                SizedBox(height: 10),
+                
+              ],
             ),
           ),
         ),

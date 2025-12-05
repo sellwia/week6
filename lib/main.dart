77d21be08b433353c9fee32194fe3f048f268f8a
sellwia <Sellydwikurnia@gmail.com>
2025-12-05 15:17:37 +0700

    Buat page splash screen awal

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:week6/screen/splash_screen_1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber)
       ),
+        home: SplashScreen1(), // panggil class layar splash
     );
   }
 }

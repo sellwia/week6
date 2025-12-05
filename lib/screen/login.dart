245d8749f6ec98645736c42f998f2ec9bab04b77
sellwia <Sellydwikurnia@gmail.com>
2025-12-05 18:24:03 +0700

    menambahkan dan membuat icon login dan text berada ditengah dengan wrap

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -6,21 +6,43 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 return Scaffold(
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(20.0),
-            child: const Column(
-              children: [
-                SizedBox(height: 10),
-                Icon(
-                  Icons.lock_outline,
-                  size: 80,
-                  color: Colors.amber,
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.all(20.0),
+              child: const Column(
+                children: [
+                  SizedBox(height: 10),
+                  Icon(
+                    Icons.lock_outline,
+                    size: 80,
+                    color: Colors.amber,
+                  ),
+          
+                  SizedBox(height: 10),
+          
+              Text(
+                "Selamat Datang",
+                style: TextStyle(fontSize: 30.0, 
+                fontWeight: FontWeight.normal
                 ),
-
-                SizedBox(height: 10),
-                
-              ],
+              ),
+          
+              SizedBox(height: 5), //jarak 20 antara lingkaran dan teks
+          
+              Text(
+                "Silahkan login untuk melanjutkan",
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.amber,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          
+              SizedBox(height: 30),
+          
+                ],
+              ),
             ),
           ),
         ),

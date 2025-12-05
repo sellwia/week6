c5774a8d320ce21f4d63e4a7e717be0982af7fc5
sellwia <Sellydwikurnia@gmail.com>
2025-12-05 15:26:04 +0700

    Buat Lingkaran

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -5,6 +5,21 @@ class SplashScreen1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+          children: [
+            SizedBox(height: 70),
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.amber,
+              ),
+            ),
+
+          ],
+        ),
+    );
   }
 }

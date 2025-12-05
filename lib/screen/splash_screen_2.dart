a41338b9962762efa3c125fca3b829e33c874f05
sellwia <Sellydwikurnia@gmail.com>
2025-12-05 16:53:52 +0700

    menambahkan 2 & 3 Pages

---
+++ lib/screen/splash_screen_2.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen2 extends StatelessWidget {
+  const SplashScreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,          
+              children: [
+              Spacer(),
+              SizedBox(height: 70),
+              Container(
+                width: 250,
+                height: 250,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.amber,
+                  image: DecorationImage(
+                  image: AssetImage("assets/images/owl.png"),
+                  fit: BoxFit.cover,
+                ),
+                ),
+              ),
+
+            SizedBox(height: 20), //jarak 20 antara lingkaran dan teks
+
+            Text(
+              "Welcome 2",
+              style: TextStyle(fontSize: 30.0, 
+              fontWeight: FontWeight.normal
+              ),
+            ),
+
+            SizedBox(height: 20), //jarak 20 antara lingkaran dan teks
+
+            Text(
+              "Forgot to bring your wallet \n when you are shopping?",
+              style: TextStyle(
+                fontSize: 15.0,
+                color: Colors.amber,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 30),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center, // vertikal tengah
+              children: [
+                Container(
+                  width: 10,
+                  height: 10,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color.fromARGB(255, 242, 152, 8),
+                  ),
+                ),
+
+                SizedBox(width: 10),
+
+                Container(
+                  width: 10,
+                  height: 10,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color.fromARGB(255, 117, 98, 57),
+                  ),
+                ),
+
+                SizedBox(width: 10),
+
+                Container(
+                  width: 10,
+                  height: 10,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color.fromARGB(255, 55, 52, 48),
+                  ),
+                ),
+                SizedBox(width: 10),
+              ],
+            ),
+
+            SizedBox(height: 30),
+
+            Container(
+            margin: EdgeInsets.only(left: 40, right: 40),
+              child: SizedBox(
+                height: 40,
+                width: double.infinity,
+                child: ElevatedButton(
+                onPressed: (){
+                  //memanggil pages selanjutnya
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 16, 237, 4),
+                  ),
+                    child: Text("Continue", style: TextStyle(fontSize: 14)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
